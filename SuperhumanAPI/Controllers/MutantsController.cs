c5d5feca12fd679fb92e6b234bcfe0648b9c53cf
energeticdynamo <jevonalex@gmail.com>
2025-08-15 11:40:37 -0400

    Adding EF Core

---
+++ SuperhumanAPI/Controllers/MutantsController.cs
@@ -0,0 +1,87 @@
+﻿using SuperhumanAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using SuperhumanAPI.Repositories;
+
+namespace SuperhumanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MutantsController : ControllerBase
+    {
+        private readonly IMutantRepository _mutantRepository;
+
+        public MutantsController(IMutantRepository mutantRepository)
+        {
+            _mutantRepository = mutantRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<Mutant>>> GetAllMutantsAsync([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            return Ok(await _mutantRepository.GetAllMutantsAsync(pageNumber, pageSize));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Mutant>> GetMutantById(int id)
+        {
+            var mutant = await _mutantRepository.GetMutantByIdAsync(id);
+
+            if (mutant == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(mutant);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Mutant>> CreateMutant(Mutant mutant)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest();
+            }
+
+            if (mutant == null)
+            {
+                return BadRequest("Mutant cannot be null");
+            }
+
+            await _mutantRepository.AddMutantAsync(mutant);
+            return CreatedAtAction(nameof(GetMutantById), new {id = mutant.MutantId}, mutant);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMutant(int id)
+        {
+            var mutant = await _mutantRepository.GetMutantByIdAsync(id);
+            if (mutant == null)
+            {
+                return NotFound();
+            }
+            
+            await _mutantRepository.DeleteMutantByMutantIdAsync(id);            
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateMutant(int id, Mutant mutant)
+        {
+            if (id != mutant.MutantId)
+            {
+                return BadRequest("Mutant ID mismatch");
+            }
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest();
+            }
+            var existingMutant = await _mutantRepository.GetMutantByIdAsync(id);
+            if (existingMutant == null)
+            {
+                return NotFound();
+            }
+            await _mutantRepository.UpdateMutantAsync(mutant);
+            return NoContent();
+        }
+    }
+}
