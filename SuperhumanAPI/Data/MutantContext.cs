8772cde1785196fc17002ffe4661ce4239c03f3d
energeticdynamo <jevonalex@gmail.com>
2025-09-12 11:00:55 -0400

    Had to refactor the program

--- SuperhumanAPI/Data/MutantContext.cs
+++ SuperhumanAPI/Data/MutantContext.cs
@@ -1,13 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SuperhumanAPI.Models;
 
-namespace SuperhumanAPI.Data
+namespace SuperhumanAPI.Data;
+
+public class MutantContext : DbContext
 {
-    public class MutantContext : DbContext
-    {
-        public MutantContext(DbContextOptions<MutantContext> options)
-            : base(options)
-        {
-        }
-        public DbSet<SuperhumanAPI.Models.Mutant> Mutants { get; set; }
-    }
+    public MutantContext(DbContextOptions<MutantContext> options) : base(options) { }
+    public DbSet<Mutant> Mutants { get; set; } = null!;
 }
--- SuperhumanAPI/Program.cs
+++ SuperhumanAPI/Program.cs
@@ -4,56 +4,30 @@ using SuperhumanAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. DEFINE a CORS policy
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost:4200") // The origin of your Angular app
-                                .AllowAnyHeader()
-                                .AllowAnyMethod();
-                      });
-});
-
-
 // Add services to the container.
-builder.Services.AddDbContext<SuperhumanContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
+// Register DbContexts
 builder.Services.AddDbContext<MutantContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddDbContext<TeamContext>(options =>
+builder.Services.AddDbContext<SuperhumanContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
+// Register Repositories
 builder.Services.AddScoped<ISuperhumanRepository, SuperhumanRepository>();
 builder.Services.AddScoped<IMutantRepository, MutantRepository>();
-builder.Services.AddScoped<ITeams, TeamsRepository>();
 
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add Swagger/OpenAPI support
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
-// 2. APPLY the CORS policy here, before Authorization and Controllers
-app.UseCors(MyAllowSpecificOrigins);
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
