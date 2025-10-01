39f9e99cab65d6f80975d6d2092c844e4e698629
energeticdynamo <jevonalex@gmail.com>
2025-10-01 16:06:18 -0400

    Adding Cosmic Beings logic

---
+++ SuperhumanAPI/Controllers/CosmicBeingController.cs
@@ -0,0 +1,78 @@
+﻿using SuperhumanAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using SuperhumanAPI.Repositories.Interfaces;
+
+namespace SuperhumanAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CosmicBeingController : ControllerBase
+    {
+        private readonly ICosmicBeingRepository _cosmicBeingRepository;
+        public CosmicBeingController(ICosmicBeingRepository cosmicBeingRepository)
+        {
+            _cosmicBeingRepository = cosmicBeingRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PagedResult<CosmicBeing>>> GetAllCosmicBeingsAsync([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+        {
+            return Ok(await _cosmicBeingRepository.GetAllCosmicBeingsAsync(pageNumber, pageSize));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CosmicBeing>> GetCosmicBeingById(int id)
+        {
+            var cosmicBeing = await _cosmicBeingRepository.GetCosmicBeingByIdAsync(id);
+            if (cosmicBeing == null)
+            {
+                return NotFound();
+            }
+            return Ok(cosmicBeing);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CosmicBeing>> CreateCosmicBeing(CosmicBeing cosmicBeing)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest();
+            }
+            if (cosmicBeing == null)
+            {
+                return BadRequest("Cosmic Being cannot be null");
+            }
+            await _cosmicBeingRepository.AddCosmicBeingAsync(cosmicBeing);
+            return CreatedAtAction(nameof(GetCosmicBeingById), new { id = cosmicBeing.CosmicId }, cosmicBeing);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCosmicBeing(int id)
+        {
+            var cosmicBeing = await _cosmicBeingRepository.GetCosmicBeingByIdAsync(id);
+            if (cosmicBeing == null)
+            {
+                return NotFound();
+            }
+
+            await _cosmicBeingRepository.DeleteCosmicBeingByCosmicIdAsync(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCosmicBeing(int id, CosmicBeing cosmicBeing)
+        {
+            if (id != cosmicBeing.CosmicId)
+            {
+                return BadRequest("Cosmic ID mismatch");
+            }
+            var existingCosmicBeing = await _cosmicBeingRepository.GetCosmicBeingByIdAsync(id);
+            if (existingCosmicBeing == null)
+            {
+                return NotFound();
+            }
+            await _cosmicBeingRepository.UpdateCosmicBeingAsync(cosmicBeing);
+            return NoContent();
+        }
+    }
+}
---
+++ SuperhumanAPI/Repositories/Interfaces/ICosmicBeingRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperhumanAPI.Models;
+namespace SuperhumanAPI.Repositories.Interfaces
+{
+    public interface ICosmicBeingRepository
+    {
+        public Task<PagedResult<CosmicBeing>> GetAllCosmicBeingsAsync(int pageNumber, int pageSize);
+        public Task<CosmicBeing> GetCosmicBeingByIdAsync(int id);
+        public Task AddCosmicBeingAsync(CosmicBeing cosmicBeing);
+        public Task UpdateCosmicBeingAsync(CosmicBeing cosmicBeing);
+        public Task<bool> DeleteCosmicBeingByCosmicIdAsync(int id);
+    }
+}
